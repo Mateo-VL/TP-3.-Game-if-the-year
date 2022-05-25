f948aff3b918d4a242dc553099bf89434a470c86
Mateo Villegas Leiva <mvillegasleiva@udesa.edu.ar>
2022-05-25 19:31:45 -0300

    Bryan Chupalo

--- Primer proyecto del trabajo.py
+++ Primer proyecto del trabajo.py
@@ -1,3 +1,11 @@
 Primer proyecto del trabajo
 Primer proyecto del trabajo
 gpltwgok5h
+
+
+def bryan_chupalo():
+    boludo = True
+    Bryan = boludo
+    while Bryan == boludo:
+        print('Bryan chupalo')
+bryan_chupalo()
