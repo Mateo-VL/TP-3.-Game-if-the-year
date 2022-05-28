179a0747d4cf538928f0be673673e63e9f3ae50c
Mateo Villegas Leiva <mvillegasleiva@udesa.edu.ar>
2022-05-28 01:39:06 -0300

    Se cambio el render de nivel a un archivo separado

--- Arrancando el TP.py
+++ Arrancando el TP.py
@@ -1,14 +1,33 @@
 import src.templates.oop.mapping as mapa
 import src.templates.oop.human as human
 import src.templates.oop.actions as actions
+import src.templates.oop.player as player
+import msvcrt
+
+DUNGEON = mapa.Dungeon(25, 80)   #pedir parametros para ver longitud y ver que no pase limites
+MATEO = human.Human('Mateo', DUNGEON.find_free_tile())
 
 
+#GNOME= player.Gnome(DUNGEON.find_free_tile())
+rows= DUNGEON.rows
+columns= DUNGEON.columns
+capo = True 
+DUNGEON.render(MATEO)  #sumar arg gnomo
+while capo:
+    key = msvcrt.getch()
+    if key == b'w':
+        actions.move_up(DUNGEON, MATEO)
+    elif key == b'a':
+        actions.move_left(DUNGEON, MATEO)
+    elif key == b's':
+        actions.move_down(DUNGEON, MATEO, rows)  #recibe cant filas
+    elif key == b'd':
+        actions.move_right(DUNGEON, MATEO, columns)  #recibe cant columnas
+    else:
+        capo = False
+    
+    DUNGEON.render(MATEO)
 
-NIVEL = mapa.Level(25, 80)
-MATEO = human.Human('Mateo', NIVEL.find_free_tile())
-#NIVEL.render(MATEO)
-actions.move_up(NIVEL, MATEO)
-NIVEL.render(MATEO)
 
 
 
