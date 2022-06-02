3cfe99bee71bbfbbd5a0d862e87b982d46e93b6b
Mateo Villegas Leiva <mvillegasleiva@udesa.edu.ar>
2022-06-02 01:29:07 -0300

    se agrego función is:connected
    se agregaron las funciones necesarias para fijarse si dos lugares están conectados. Falta testear, corregir y comprobar su correcto funcionamiento

--- src/templates/oop/mapping.py
+++ src/templates/oop/mapping.py
@@ -1,13 +1,13 @@
-import random
-from typing import Optional
-from typing import List, Tuple
-
+from __future__ import annotations
 import src.templates.oop.player as player
 import src.templates.oop.items as items
+import random
+from typing import Optional
+from typing import List, Tuple, Set
+from copy import copy
 
 
 Location = Tuple[int, int]
-
 
 class Tile:
     """Tile(char: str, walkable: bool=True)
@@ -182,10 +182,50 @@ class Level:
 
         raise NotImplementedError
 
-    def are_connected(self, initial: Location, end: Location) -> bool:
-        """Check if there is walkable path between initial location and end location."""
-        # completar
-        raise NotImplementedError
+    def are_connected(self,level: Level, initial: Location, end: Location) -> bool:
+        return search_path(level, initial, end, set())
+        
+    def is_inside_map(rows: int, columns: int, point: Location) -> bool:
+        if point[0] < 0 or point[0] >= rows:
+            return False
+        if point[1] < 0 or point[1] >= columns:
+            return False
+        return True
+
+    def get_neighbours(level: Level, point: Location) -> List[Location]:
+        directions = {
+        '0': [1, 0],
+        '90': [0, -1],
+        '180': [-1, 0],
+        '270': [0, 1]
+        }
+        neighbours = []
+        for direction in directions:
+            possible_neighbour = (point[0] + directions[direction][0], point[1] + directions[direction][1])
+            if is_inside_map(level.rows, level.columns, possible_neighbour):
+                neighbours.append(possible_neighbour)
+        return neighbours
+
+    def search_path(level : Level, initial: Location, end: Location, visited: Set) -> bool:
+        if initial == end:
+            return True
+        found = False
+        for point in get_neighbours(level, initial):
+            if is_available(level, point, visited):
+                visited.add(point)
+                current_visited = copy(visited)
+                found = search_path(level, point, end, visited)
+            if found:
+                break
+        return found
+    
+    
+    def is_available(level: Level, point: Location, visited: Set) -> bool:
+        if point in visited:
+            return False
+        if not level.is_walkable(point):
+            return False
+        return True
 
     def get_path(self, initial: Location, end: Location) -> bool:
         """Return a sequence of locations between initial location and end location, if it exits."""
