1e5146036a1d178091fb10fe3d9617bc25015d56
Mateo Villegas Leiva <mvillegasleiva@udesa.edu.ar>
2022-06-06 19:46:16 -0300

    p

--- src/templates/oop/game.py
+++ src/templates/oop/game.py
@@ -5,29 +5,23 @@ import player
 import items         
 import hay_camino
 
-ROWS = 25
-COLUMNS = 80
-player_name = input('Enter your name: ')
-DUNGEON = mapping.Dungeon(ROWS, COLUMNS)   #pedir parametros para ver longitud y ver que no pase limites
-PLAYER = human.Human(player_name, DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_UP))
-GNOMES = [player.Gnome('Gnome', DUNGEON.find_free_tile()) for _ in range(len(DUNGEON.dungeon))]
-
-SWORD= items.Sword("Sword", "/", 10, 20, DUNGEON.find_free_tile())
-AMULET= items.Amulet("Amulet", '"', DUNGEON.dungeon[2].find_free_tile())
-PICKAXE= items.PickAxe("Pickaxe", "(", DUNGEON.find_free_tile())
-
-rows= DUNGEON.rows
-columns= DUNGEON.columns
 
 if __name__ == "__main__":
+    ROWS = 25
+    COLUMNS = 80
+    DUNGEON = mapping.Dungeon(ROWS, COLUMNS)   #pedir parametros para ver longitud y ver que no pase limites
+    PLAYER = human.Human(DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_UP))
+    GNOMES = [player.Gnome('Gnome', DUNGEON.find_free_tile()) for _ in range(len(DUNGEON.dungeon))]
+    
+    SWORD= items.Sword("Sword", "/", 10, 20, DUNGEON.dungeon[1].find_free_tile())
+    AMULET= items.Amulet("Amulet", '"', DUNGEON.dungeon[2].find_free_tile())
+    PICKAXE= items.PickAxe("Pickaxe", "(", DUNGEON.find_free_tile())
     game = True 
     DUNGEON.add_item(PICKAXE, 1, PICKAXE.loc())
-    DUNGEON.add_item(SWORD, 1, SWORD.loc())
+    DUNGEON.add_item(SWORD, 2, SWORD.loc())
     DUNGEON.add_item(AMULET, 3, AMULET.loc())
     turns=0
     DUNGEON.render(PLAYER, GNOMES[DUNGEON.level], DUNGEON.level, turns)
-    
-    print(DUNGEON.dungeon[DUNGEON.level].are_connected(DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_UP), DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_DOWN)))
     while game and PLAYER.alive==True:
         turns +=1
         
@@ -36,8 +30,8 @@ if __name__ == "__main__":
         DUNGEON.render(PLAYER, GNOMES[DUNGEON.level], DUNGEON.level, turns)
         print(DUNGEON.dungeon[DUNGEON.level].are_connected(DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_UP), DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_DOWN)))
     if PLAYER.treasure == True:
-        print("You won in {} turns".format(turns))
+        print('CONGRATULATIONS ', PLAYER, ', YOU HAVE FOUND THE TREASURE')
     else:
-        print("You lost in {} turns".format(turns))
+        print('YOU HAVE BEEN DEFEATED', PLAYER, 'BETTER LUCK NEXT TIME')
 
 
