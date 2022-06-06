1d5e3ec9944dce81516782c3d87ba91fcc39f4b8
Bryan Drexler <bdrexler@udesa.edu.ar>
2022-06-06 16:26:15 -0300

    Auto stash before merge of "dr" and "origin/dr"

--- src/templates/oop/game.py
+++ src/templates/oop/game.py
@@ -2,7 +2,10 @@ import mapping
 import human
 import actions
 import player
-import items         
+import items         #cambiar los imports para entrega final
+#from src.templates.oop.human import Human
+#from src.templates.oop.items import Item
+#import src.templates.oop.actions as actions
 import hay_camino
 
 ROWS = 25
@@ -22,19 +25,18 @@ if __name__ == "__main__":
     #player_name= input("Name:")
     game = True 
     DUNGEON.add_item(PICKAXE, 1, PICKAXE.loc())
-    DUNGEON.add_item(SWORD, 1, SWORD.loc())
+    DUNGEON.add_item(SWORD, 2, SWORD.loc())
     DUNGEON.add_item(AMULET, 3, AMULET.loc())
     turns=0
     DUNGEON.render(PLAYER, GNOMES[DUNGEON.level], DUNGEON.level, turns)
     
-    print(hay_camino.are_connected(DUNGEON, DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_UP), DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_DOWN)))    
     while game and PLAYER.alive==True:
+        #if hay_camino.are_connected(DUNGEON, DUNGEON[DUNGEON.level].index())
         turns +=1
         
         game = actions.use_turn(PLAYER, GNOMES[DUNGEON.level], DUNGEON, game)
         PLAYER.pickup(DUNGEON.get_items(PLAYER.loc()))      
-        DUNGEON.render(PLAYER, GNOMES[DUNGEON.level], DUNGEON.level, turns)
-        print(hay_camino.are_connected(DUNGEON, DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_UP), DUNGEON.dungeon[DUNGEON.level].index(mapping.STAIR_DOWN)))
+        DUNGEON.render(PLAYER, GNOMES[DUNGEON.level], DUNGEON.level)
     if PLAYER.treasure == True:
         print("You won in {} turns".format(turns))
     else:
